d0dac754a88036a6d0d0df64f54665d585590256
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-10-19 22:04:13 +0500

    method

---
+++ app.js
@@ -0,0 +1,22 @@
+// let numbers = [1, 2, 3, 4];
+// let doubledNumbers = numbers.map(function(num) {
+//     return num  * 2;
+//   });
+// console.log(doubledNumbers);
+// console.log(numbers);
+
+// let numbers = [1, 2, 3, 4];
+// numbers.forEach(function(value,index) {
+//     console.log(value,index);
+//   });
+
+
+
+let numbers = [4, 2, 5, 1, 3];
+numbers.sort(); // [1, 2, 3, 4, 5] - string comparison
+console.log(numbers);
+
+
+let ma = [2,4,5,6,7,8,1,9,10];
+ma.sort();
+console.log(ma);
